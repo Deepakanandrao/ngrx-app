6ecc3ff199021b12b0a48e8df1ed7138a71c0565
deep.anandrao@gmail.com <deep.anandrao@gmail.com>
2020-09-27 22:52:59 -0400

    ngrx working condition

--- src/app/my-counter/store/counter.actions.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@ngrx/store';
-
-// export actions here
-
-export const increment = createAction('Increment');
-export const decrement = createAction('Decrement');
-export const reset = createAction('Reset');
